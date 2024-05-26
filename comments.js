971104786b7be1d44f8f1c3d5e69c41cce3f93ad
Anubhav Shavaran <91604480+anubhavshavaran@users.noreply.github.com>
2024-05-26 17:55:14 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,21 @@
+// Create a web server
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/comments') {
+        const comments = [
+            { "author": "Anne Droid", "comment": "I wanna know what love is..." },
+            { "author": "Billy Idol", "comment": "Eyes without a face..." },
+            { "author": "Cal Jammer", "comment": "Chillin' like a villain..." }
+        ];
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ comments }));
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
